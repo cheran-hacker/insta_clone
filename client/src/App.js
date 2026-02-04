b65636a991ac78b124cd39e10891f8b8e2ff235d
cheran-hacker <cheransuresh869@gmail.com>
2026-02-04 20:37:49 +0530

    second

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useReducer, useContext } from 'react';
-import Navbar from './components/Navbar';
-import "./App.css";
-import { BrowserRouter, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Profile from './screens/Profile';
-import CreatePost from './screens/CreatePost';
-import { reducer, initialState } from './reducers/userReducer';
-import UserProfile from './screens/UserProfile';
-import Archive from './screens/Archive';
-import Messenger from './screens/Messenger';
-import Explore from './screens/Explore';
-import { UserContext } from './context/UserContext';
-import Sidebar from './components/Sidebar';
+import React, { useEffect, useReducer, useContext } from "react";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
-const Routing = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/create" element={<CreatePost />} />
-      <Route path="/archive" element={<Archive />} />
-      <Route path="/profile/:userid" element={<UserProfile />} />
-      <Route path="/chat" element={<Messenger />} />
-      <Route path="/explore" element={<Explore />} />
-    </Routes>
-  );
-};
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import Profile from "./screens/Profile";
+import UserProfile from "./screens/UserProfile";
+import CreatePost from "./screens/CreatePost";
+import Archive from "./screens/Archive";
+import Messenger from "./screens/Messenger";
+import Explore from "./screens/Explore";
+
+import { reducer, initialState } from "./reducers/userReducer";
+import { UserContext } from "./context/UserContext";
+
+import "./App.css";
+
+/* -------- ROUTES -------- */
+
+const Routing = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/profile/:userid" element={<UserProfile />} />
+    <Route path="/create" element={<CreatePost />} />
+    <Route path="/archive" element={<Archive />} />
+    <Route path="/chat" element={<Messenger />} />
+    <Route path="/explore" element={<Explore />} />
+  </Routes>
+);
+
+/* -------- APP -------- */
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -43,7 +49,8 @@ function App() {
   );
 }
 
-// App Content 
+/* -------- APP CONTENT -------- */
+
 const AppContent = () => {
   const { dispatch } = useContext(UserContext);
   const navigate = useNavigate();
@@ -52,20 +59,20 @@ const AppContent = () => {
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     const token = localStorage.getItem("jwt");
-    const theme = localStorage.getItem("theme");
-
-    if (theme === "dark") document.body.classList.add("dark-mode");
 
     if (user && token) {
       dispatch({ type: "USER", payload: user });
-    } else {
-      if (!location.pathname.startsWith('/reset') && location.pathname !== '/signup' && location.pathname !== '/login') {
-        navigate('/login');
-      }
+    } else if (
+      location.pathname !== "/login" &&
+      location.pathname !== "/signup"
+    ) {
+      navigate("/login");
     }
   }, [dispatch, navigate, location.pathname]);
 
-  const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
+  const isAuthPage =
+    location.pathname === "/login" ||
+    location.pathname === "/signup";
 
   return (
     <div className={!isAuthPage ? "app-layout" : ""}>
